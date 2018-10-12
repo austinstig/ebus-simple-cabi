00cb01fb731d87deda4a0514bac90ed7a490ef1e
austin <austinstig@gmail.com>
2018-10-12 07:58:19 -0500

    remove pleora.rs

--- pleora.rs
+++
@@ -1,8 +0,0 @@
-use libc::{c_int, c_char};
-
-#[link(name="wrapper", kind="dylib")]
-extern {
-    #[repr(C)]
-    pub struct EBUSState { _unused: [u8;0] }
-    pub fn mkstate() -> *mut EBUSState;
-}
