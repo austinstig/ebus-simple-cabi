2ffd1242f88a9ecc35bc63ce3b064be304e60e08
Go Namhyeon <gnh1201@gmail.com>
2021-07-18 09:23:02 +0000

    Update build.sh

--- ebus-wrapper/build.sh
+++ ebus-wrapper/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 g++ -c -I/opt/pleora/ebus_sdk/Ubuntu-18.04-x86_64/include -I.  -O3 -D_UNIX_ -D_LINUX_ -DQT_GUI_LIB -fPIC -std=c++11 -o wrapper.o wrapper.cpp
 #g++ -shared -fPIC -o wrapper.so wrapper.o
-g++ -shared wrapper.o -o wrapper.so -L/opt/pleora/ebus_sdk/Ubuntu-18.04-x86_64/lib -lPvAppUtils -lPtConvertersLib -lPvBase -lPvBuffer -lPvGenICam -lPvSystem -lPvStream -lPvDevice -lPvTransmitter -lPvVirtualDevice -lPvPersistence -lPvSerial -lPvCameraBridge -lPvGUI -lSimpleImagingLib -L../../../lib/genicam/bin/Linux64_x64
+g++ -shared wrapper.o -o wrapper.so -L/opt/pleora/ebus_sdk/Ubuntu-18.04-x86_64/lib -lPvAppUtils -lPtConvertersLib -lPvBase -lPvBuffer -lPvGenICam -lPvSystem -lPvStream -lPvDevice -lPvTransmitter -lPvVirtualDevice -lPvPersistence -lPvSerial -lPvCameraBridge -lPvGUI -lSimpleImagingLib -L/opt/pleora/ebus_sdk/Ubuntu-18.04-x86_64/lib/genicam/bin/Linux64_x64
 #sudo cp -f wrapper.so /usr/local/lib/libEBUSWrapper.so
 echo "/home/myworld/Github/ebus-simple-cabi/ebus-wrapper" > /etc/ld.so.conf.d/ebus_wrapper.conf
 ldconfig
